356d005e5d7c6913551fff94e07dfc8aa03eac1d
Jorge <jogonzal@microsoft.com>
2017-05-14 15:45:34 -0700

    Include test and train data on repo

--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -1,11 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Homework3.Parsing;
 
 namespace Homework3
 {
 	class Program
 	{
-		private static string TrainingDataPath => Path.Combine(Directory.GetCurrentDirectory() + @"\..\..\..\..\netflix_data\movie_titles.txt");
-		private static string TestDataPath => Path.Combine(Directory.GetCurrentDirectory() + @"\..\..\..\..\netflix_data\trainingRatings.txt");
+		private static string TrainingDataPath => Path.Combine(Directory.GetCurrentDirectory() + @"\..\..\..\..\data\train");
+		private static string TestDataPath => Path.Combine(Directory.GetCurrentDirectory() + @"\..\..\..\..\data\test");
 
 		static void Main(string[] args)
 		{
@@ -18,8 +20,29 @@ namespace Homework3
 			{
 				errorMessage += $"Failed to find file ${TestDataPath} - please update variable ${nameof(TestDataPath)} or create that file.\n";
 			}
+			if (errorMessage != "")
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Not all files available - not running!");
+				Console.WriteLine(errorMessage);
+				Console.ResetColor();
+				Console.WriteLine("Press any key to continue...");
+				Console.ReadKey();
+				return;
+			}
 
+			var startTime = DateTime.Now;
+			Console.WriteLine(startTime);
 
+			var trainingEmails = CsvParserUtils.ParseEmailExamples(TrainingDataPath);
+			var testEmails = CsvParserUtils.ParseEmailExamples(TestDataPath);
+
+			var endTime = DateTime.Now;
+			Console.WriteLine(endTime);
+			var totalMinutes = (endTime - startTime).TotalMinutes;
+			Console.WriteLine("Took {0} minutes.", totalMinutes);
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
 		}
 	}
 }
